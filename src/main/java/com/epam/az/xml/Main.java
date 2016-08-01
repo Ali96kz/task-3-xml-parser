90e1d1503d96363cd028f68325f346ed26443054
Ali96kz <jakparov.kz@gmail.com>
2016-08-02 00:59:09 +0600

    Initial commit

---
+++ src/main/java/com/epam/az/xml/Main.java
@@ -0,0 +1,10 @@
+package com.epam.az.xml;
+
+public class Main {
+    public static void main(String[] args) {
+        //TODO create xsd schema
+        //TODO create xml example
+        //TODO create entity
+        //TODO add Jaxb
+    }
+}
---
+++ src/main/java/com/epam/az/xml/entity/Flower.java
@@ -0,0 +1,16 @@
+package com.epam.az.xml.entity;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "person")
+@XmlType(propOrder = {"name","soil","origin"})
+public class Flower {
+    private String name;
+    private String soil;
+    private String origin;/*
+    private String visualParametrs;
+    private String growingTips;
+    private String multiplying;*/
+
+}
