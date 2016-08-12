573a21cedc65ce2b973d5e98aee010515252cf7b
Ali96kz <jakparov.kz@gmail.com>
2016-08-12 23:44:24 +0600

    optimize FlowerXmlParser

--- src/main/java/com/epam/az/xml/parser/FlowersXmlParser.java
+++ src/main/java/com/epam/az/xml/parser/FlowersXmlParser.java
@@ -8,29 +8,47 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 
-public abstract class FlowersXmlParser implements XmlParser{
+public abstract class FlowersXmlParser implements XmlParser {
     protected StringBuilder stringBuilder = new StringBuilder();
     protected Class clazz;
     protected Method method;
-    FlowerStack flowerStack = new FlowerStack();
     //TODO get this parameters from root class
-    String rootName = "flower", listName = "flowers";
-    Class rootClass = AliveFlower.class;
 
-    protected void elementStart(String qName){
+    String rootItem = "flower", root = "flowers";
+    FlowerStack flowerStack = new FlowerStack();
+    Class rootItemClass = AliveFlower.class;
+    Object rootInstance ;
+    Method addObjectInList;
+
+    public void configureParser(Class rootClass, String rootItem) {
         try {
-            if (qName.equalsIgnoreCase(rootName)) {
+
+            Class returnType= rootClass.getMethod("get"+rootItem, null).getReturnType();
+            addObjectInList = rootClass.getDeclaredMethod("add" + rootItem, returnType);
+            rootInstance = rootClass.newInstance();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    protected void elementStart(String qName) {
+        try {
+            if (qName.equalsIgnoreCase(rootItem)) {
                 flowerStack = new FlowerStack();
-                Object item=  rootClass.newInstance();
+                Object item = rootItemClass.newInstance();
                 flowerStack.push(item);
-            } else if (qName.equalsIgnoreCase(listName)) {
+            } else if (qName.equalsIgnoreCase(root)) {
 
             } else {
-
                 Method getMethod = flowerStack.getLast().getClass().getMethod("get" + upFirstChar(qName), null);
                 method = flowerStack.getLast().getClass().getMethod("set" + upFirstChar(qName), getMethod.getReturnType());
                 clazz = getMethod.getReturnType();
-                if (!clazz.isPrimitive() && clazz != Object.class && clazz != String.class) {
+                if (!clazz.isPrimitive() && clazz != String.class) {
                     pushObjectInStack(clazz.getDeclaredMethods(), clazz.newInstance(), flowerStack);
                 }
             }
@@ -40,19 +58,21 @@ public abstract class FlowersXmlParser implements XmlParser{
         }
 
     }
-    protected void chars(String string){
+
+    protected void chars(String string) {
         stringBuilder.append(string);
     }
-    protected void elementEnd(String qName, GreenHouse greenHouse){
-        if (qName.equalsIgnoreCase(rootName)) {
-            greenHouse.add((AliveFlower) flowerStack.pop());
-        } else if (qName.equalsIgnoreCase(listName)) {
+
+    protected void elementEnd(String qName, GreenHouse greenHouse) {
+        if (qName.equalsIgnoreCase(rootItem)) {
+            addInRootList(flowerStack.pop());
+        } else if (qName.equalsIgnoreCase(root)) {
 
         } else if (qName.equalsIgnoreCase(flowerStack.getLast().getClass().getSimpleName())) {
             Object value = flowerStack.pop();
             invokeSetterForObject(flowerStack.pop(), value);
         } else {
-            String characterValue = stringBuilder.toString().replaceAll("\\s","");
+            String characterValue = stringBuilder.toString().replaceAll("\\s", "");
             Object value = stringToObjectType(characterValue, clazz);
             invokeSetterForPrimitive(flowerStack.pop(), value);
         }
@@ -67,16 +87,22 @@ public abstract class FlowersXmlParser implements XmlParser{
             } catch (NumberFormatException e) {
             }
         }
-        if(aclass.getSimpleName() == "boolean"){
-            if(str.charAt(0) == 't' && str.charAt(1) == 'r' && str.charAt(2) == 'u' && str.charAt(3) == 'e'){
+        if (aclass.getSimpleName().equalsIgnoreCase("boolean")) {
+            if (str.equalsIgnoreCase("true")) {
                 return true;
-            }else {
+            } else {
                 return false;
             }
         }
         return stringBuilder.toString();
     }
-
+    private void addInRootList(Object value){
+        try {
+            addObjectInList.invoke(rootInstance, value);
+        } catch (IllegalAccessException | InvocationTargetException  e) {
+            e.printStackTrace();
+        }
+    }
     protected void pushObjectInStack(Method[] methods, Object object, FlowerStack flowerStack) {
         for (Method method1 : methods) {
             if (method1.getName().startsWith("get")) {
@@ -93,7 +119,8 @@ public abstract class FlowersXmlParser implements XmlParser{
             e.printStackTrace();
         }
     }
-    protected void invokeSetterForObject(Object inputClass, Object value){
+
+    protected void invokeSetterForObject(Object inputClass, Object value) {
         try {
             Method getMethod = inputClass.getClass().getMethod("get" + value.getClass().getSimpleName(), null);
             Method setMethod = inputClass.getClass().getMethod("set" + value.getClass().getSimpleName(), getMethod.getReturnType());
